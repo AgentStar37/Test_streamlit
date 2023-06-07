a1984b7e297b52862fef9c5494d6ebfafb9b0e93
Tanguy Collet <tanguy.collet2712@gmail.com>
2023-06-07 12:00:30 +0200

    initial installation

---
+++ test_snowflake_streamlit/main.py
@@ -0,0 +1,19 @@
+from snowflake.snowpark.session import Session
+from snowflake.snowpark.functions import avg, sum, col,lit
+import streamlit as st
+import pandas as pd
+
+# Create Session object
+def create_session_object():
+   connection_parameters = {
+      "account": "<account_identifier>",
+      "user": "<username>",
+      "password": "<password>",
+      "role": "<role_name>",
+      "warehouse": "<warehouse_name>",
+      "database": "ENVIRONMENT_DATA_ATLAS",
+      "schema": "ENVIRONMENT"
+   }
+   session = Session.builder.configs(connection_parameters).create()
+   return session
+
