ff26779e300a7b3a075818ee74fa91583cc18bb6
Tanguy Collet <tanguy.collet2712@gmail.com>
2023-06-07 16:50:38 +0200

    essai de divider

--- Projet/Main.py
+++ Projet/Main.py
@@ -35,7 +35,8 @@ st.markdown(
             background-position: 20px 20px;
         }
         [data-testid="stSidebarNav"]::before {
-            content: "Menu des pages de l'appli";
+            content: "Menu des pages de l'appli
+            -------------------------";
             margin-left: 20px;
             margin-top: 20px;
             font-size: 20px;
