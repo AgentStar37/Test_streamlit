c2c455352116404089a87fa36436fc516f1ba762
Tanguy Collet <tanguy.collet2712@gmail.com>
2023-06-07 09:13:59 +0200

    add spacing

--- Projet/pages/3_About.py
+++ Projet/pages/3_About.py
@@ -5,6 +5,7 @@ st.title("A propos de cette application")
 st.write("Cette application a été réalisée au cours d'un stage par un étudiant à Polytech Tours")
 
 st.text("")
+st.text("")
 
 st.write("Etes vous satisfait de l'application ?")
 note = st.slider("Donnez une note !",0,5)
