eaf233dfa0be1da2123d942a75da320433379482
Tanguy Collet <tanguy.collet2712@gmail.com>
2023-06-08 09:26:07 +0200

    got rid of fonctions

--- Projet/pages/2_Flight_condition.py
+++ Projet/pages/2_Flight_condition.py
@@ -19,8 +19,27 @@ st.set_page_config(
     )
 #--------------------------------------
 
-from fonctions import *
-f()
+st.markdown(
+    """
+    <style>
+        [data-testid="stSidebarNav"] {
+            padding-top: 150px;
+            background-position: 20px 20px;
+        }
+        [data-testid="stSidebarNav"]::before {
+            content: "Menu des pages de l'appli";
+            margin-left: 20px;
+            margin-top: 20px;
+            font-size: 20px;
+            position: relative;
+            top: 70px;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+    )
+
+st.sidebar.divider()
 
 st.title("Conditions de vol:")
 
--- Projet/pages/3_About.py
+++ Projet/pages/3_About.py
@@ -7,8 +7,27 @@ st.set_page_config(
     )
 #--------------------------------------
 
-from fonctions import *
-f()
+st.markdown(
+    """
+    <style>
+        [data-testid="stSidebarNav"] {
+            padding-top: 150px;
+            background-position: 20px 20px;
+        }
+        [data-testid="stSidebarNav"]::before {
+            content: "Menu des pages de l'appli";
+            margin-left: 20px;
+            margin-top: 20px;
+            font-size: 20px;
+            position: relative;
+            top: 70px;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+    )
+
+st.sidebar.divider()
 
 st.title("A propos de cette application")
 
